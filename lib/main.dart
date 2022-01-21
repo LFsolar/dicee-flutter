ba5a897b7136dee90d54a4a20937c60e92f88f1c
Lisa Sun <sunlisa@gmail.com>
2022-01-21 01:22:03 -0600

    dice reactive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -23,21 +24,23 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    int leftDiceNumber = 3;
     return Center(
       child: Row(
-        children: [
+        children: <Widget>[
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
                 onPressed: () => {
                   setState(() {
-                    leftDiceNumber = 5;
-                    print('textbutton pressed');
-                  })
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    print('leftDiceNumber = $leftDiceNumber');
+                  }),
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
@@ -47,8 +50,13 @@ class _DicePageState extends State<DicePage> {
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: TextButton(
-                onPressed: () => print('textbutton pressed'),
-                child: Image.asset('images/dice1.png'),
+                onPressed: () => {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                    print('rightDiceNumber = $rightDiceNumber');
+                  }),
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
             ),
           ),
